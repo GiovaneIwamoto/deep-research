1ee38b12c5ea025bfac130d28251c16faadb4ff9
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-08-18 05:21:55 -0400

    refactor: search summarizer service

--- src/services/search_summarizer.py
+++ src/services/search_summarizer.py
@@ -3,61 +3,56 @@ Search and summarize services for the Deep Research Agent project.
 """
 
 import logging
-from models.base import QueryResult
-from templates.prompt_web_search_summarizer import web_search_summarizer_prompt
-from config.system_config import Configuration
-from langchain_core.runnables import RunnableConfig
+from typing import Dict, Any
 from tavily import TavilyClient
 from langchain_openai import ChatOpenAI
-from typing import Dict, Any
+from langchain_core.runnables import RunnableConfig
 
+from models.base import QueryResult
+from utils.current_date import get_current_date
+from config.system_config import Configuration
+from templates.prompt_web_search_summarizer import web_search_summarizer_prompt
 
 def search_and_summarize(data: Dict[str, Any], config: RunnableConfig) -> Dict[str, Any]:
-    """
-    Perform a web search for the given query, extract content, and summarize it using an LLM.
-    This function represents a single agent that handles both search and summarization tasks.
-    """
-    # Get the query and current date from the data
+    """Perform a web search for the given query, extract content, and summarize it using an LLM."""
+    configurable = Configuration.from_runnable_config(config)
+    
+    # Get query
     query = data["query"]
-    current_date = data["current_date"]
 
-    # Initialize the Tavily client
+    # Tavily search
     tavily_client = TavilyClient()
-
-    # Search for the query
     results = tavily_client.search(query, max_results=1, include_raw_content=False)
 
-    # Extract the url from the results
-    url = results["results"][0]["url"]
-    url_extraction = tavily_client.extract(url)
+    # Extract url link and content
+    url_link = results["results"][0]["url"]
+    url_content = tavily_client.extract(url_link)
+    
+    # If the content is not empty summarize it
+    if len(url_content["results"]) > 0:
+        raw_content = url_content["results"][0]["raw_content"]
 
-    # Use configuration from RunnableConfig
-    configurable = Configuration.from_runnable_config(config)
+        # Configure model
+        summarizer_llm = ChatOpenAI(model=configurable.summarization_model)
 
-    # Summarize the content using an LLM
-    summarizer_llm = ChatOpenAI(model=configurable.summarization_model)
-
-    # If the content is not empty, summarize it using an LLM
-    if len(url_extraction["results"]) > 0:
-        raw_content = url_extraction["results"][0]["raw_content"]
-
-        prompt = web_search_summarizer_prompt.format(current_date=current_date, page_content=raw_content)
-        llm_result = summarizer_llm.invoke(prompt)
+        # Invoke model
+        prompt = web_search_summarizer_prompt.format(current_date=get_current_date(), page_content=raw_content)
+        response = summarizer_llm.invoke(prompt)
         
-        # Create the query result
+        # Create query result object
         query_results = QueryResult(
             title=results["results"][0]["title"],
-            url=url,
-            summary=llm_result.content
+            url=url_link,
+            summary=response.content
         )
-        logging.info(f"QUERY: {query}\n\n[SEARCH RESULT SUMMARY]:\n\n{query_results}\n\n")
+        logging.info(f"Query: {query}\n\n[Search Result Summary]:\n\n{query_results}\n\n")
 
     else:
         query_results = QueryResult(
             title=results["results"][0]["title"],
-            url=url,
+            url=url_link,
             summary="No content could be extracted from the provided URL."
         )
-        logging.warning(f"[NO CONTENT EXTRACTED FROM QUERY]: {query}\n(URL: {url})\n\n")
+        logging.warning(f"[No content extracted from query]: {query}\n(URL: {url_link})\n\n")
 
-    return {"queries_results": [query_results]} 
+    return {"queries_results": [query_results]}
